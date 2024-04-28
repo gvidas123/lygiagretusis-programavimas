0811fc79be5d33e7c8c384c9f8bf378bfea0e9d0
gvidas123 <93709395+gvidas123@users.noreply.github.com>
2024-04-28 20:48:59 +0300

    Update Main1.java

--- Main1.java
+++ Main1.java
@@ -1,5 +1,5 @@
 import java.util.Random;
-public class Main {
+public class Main1 {
     public static void main(String[] args) {
         System.out.println("Programa pradeda darba");
         chess_thread.pradeti();
